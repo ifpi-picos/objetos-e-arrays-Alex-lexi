8addc7962d994f03ff1a639975f7f9540b5c0928
Alex-lexi <capic.2024118tads0015@aluno.ifpi.edu.br>
2024-08-08 16:38:58 -0300

    atividade

---
+++ import3.js
@@ -0,0 +1,7 @@
+const operacoes = require('./ex03.js');
+console.log(operacoes.soma(5, 3));
+console.log(operacoes.subtracao(10, 5));
+console.log(operacoes.multiplicacao(8 ,5));
+console.log(operacoes.divisao(100, 20))
+console.log(operacoes.exponenciacao(2, 2));
+
---
+++ import4.js
@@ -0,0 +1,12 @@
+import { soma, subtracao, multiplicacao, divisao, exponenciacao } from './ex04.js'
+const resultadoSoma = soma (10, 20);
+const resultadoSubtracao = subtracao (90,5);
+const resultadoMultiplicacao = multiplicacao(3, 5);
+const resultadoDivisao = divisao (100, 2);
+const resultadoExponenciacao = exponenciacao (2,2);
+
+console.log(resultadoSoma);
+console.log(resultadoSubtracao);
+console.log(resultadoMultiplicacao);
+console.log(resultadoDivisao);
+console.log(resultadoExponenciacao);
